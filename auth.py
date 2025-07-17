46d82c5e33e6a9898d766d617f1054e6044ee1da
Muhammad Sami Ullah-Business Analyst <165421205+SamiAheer786@users.noreply.github.com>
2025-07-17 12:48:18 +0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,4 +1,3 @@
-# Hardcoded credentials
 users = {
     "student1": {"password": "student123", "role": "Student"},
     "supervisor1": {"password": "super123", "role": "Supervisor"},
@@ -7,5 +6,11 @@ users = {
 def login(username, password):
     user = users.get(username)
     if user and user["password"] == password:
+        return user["role"]
+    return None
+
+def get_user_role(username):
+    user = users.get(username)
+    if user:
         return user["role"]
     return None
